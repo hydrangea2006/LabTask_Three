7d282b88acee3b3bcd081eef98eb89c1725be2d6
cjc258076 <3760815750@qq.com>
2026-04-25 18:21:12 +0800

    feat: 添加友元函数、函数对象和动态内存分配
    - 新增友元函数 showPowerInfo()
    - 新增函数对象 PowerCalculator
    - 新增动态内存分配存储计算结果

--- taylor_logic.h
+++ taylor_logic.h
@@ -8,7 +8,7 @@ class Taylor_Logic
 public:
     Taylor_Logic();
 
-    // 三个递归函数
+    // 三个递归函数 - 保持原样，不加 const
     double factorial(int num);
     double power(int x, int exp);
     double taylorSeries(int x, int n, int currentTerm);
@@ -23,6 +23,47 @@ public:
     QString showTaylorSteps(int x, int n, int currentTerm);
     QString showPowerSteps(int x, int exp);
     QString showFactorialSteps(int num);
+
+    // ========== 新增 ==========
+    // 注意：这里不是 const 引用
+    friend double calculateErrorRate(Taylor_Logic& logic, int x, int n);
+
+    double calculateWithFunctor(int x, int n);
+    void addToHistory(int x, int n, double result);
+    QString getHistory() const;
+    void clearHistory();
+
+private:
+    struct HistoryRecord {
+        int x;
+        int n;
+        double result;
+        double exactValue;
+        HistoryRecord* next;
+    };
+    HistoryRecord* head;
 };
+
+// 函数对象
+class FactorialFunctor {
+public:
+    double operator()(int num);
+};
+
+class PowerFunctor {
+private:
+    int base;
+public:
+    explicit PowerFunctor(int b);
+    double operator()(int exp);
+};
+
+class PrecisionComparator {
+public:
+    QString operator()(double result, double exactValue);
+};
+
+// 友元函数声明 - 注意：没有 const
+double calculateErrorRate(Taylor_Logic& logic, int x, int n);
 
 #endif
